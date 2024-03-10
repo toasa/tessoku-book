bf5bcd71e2334b541ba3f552b3552cf76784f473
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-03-10 18:02:09 +0900

    Add segment tree

---
+++ toybox/segment_tree.py
@@ -0,0 +1,94 @@
+from math import log2
+
+
+class Monoid:
+    def __init__(self, _set, op, id_elem):
+        self.set = _set
+        self.op = op
+        self.id_elem = id_elem
+
+
+class SegmentTree:
+    def __init__(self, monoid):
+        self.monoid = monoid
+
+        self.nleaf = SegmentTree.__nleaf(len(monoid.set))
+        self.leaf_start_i = self.nleaf-1
+
+        tree_len = 2**(int(log2(self.nleaf))+1)-1
+        self.tree = [monoid.id_elem] * tree_len
+
+        for i in range(len(monoid.set)):
+            self.tree[self.leaf_start_i+i] = monoid.set[i]
+        for i in range(self.nleaf-2, -1, -1):
+            self.tree[i] = self.monoid.op(
+                self.tree[SegmentTree.__left(i)],
+                self.tree[SegmentTree.__right(i)],
+            )
+
+        # print(self.tree)
+
+    def update(self, i, val):
+        # モノイドのインデックスをセグメント木のインデックスへ変換
+        i = i + self.leaf_start_i
+
+        self.tree[i] = val
+
+        # Update up to root.
+        p = SegmentTree.__parent(i)
+        while p != None:
+            self.tree[p] = self.monoid.op(
+                self.tree[SegmentTree.__left(p)],
+                self.tree[SegmentTree.__right(p)],
+            )
+            p = SegmentTree.__parent(p)
+
+    # 半開区間 [l, r) の要素に対し、モノイド積を適用した結果を返す
+    def answer_range(self, l, r):
+        l_i = l+self.leaf_start_i
+        r_i = r-1+self.leaf_start_i
+
+        if r - l == 1:
+            return self.tree[l_i]
+
+        while l_i % 2 == 1:
+            l_i = SegmentTree.__parent(l_i)
+
+        # Root ノードまでは遡らない
+        while r_i % 2 == 0 and SegmentTree.__parent(r_i) != 0:
+            r_i = SegmentTree.__parent(r_i)
+
+        return self.monoid.op(
+            self.tree[l_i],
+            self.tree[r_i],
+        )
+
+    def __nleaf(n):
+        for i in range(1, 33):
+            if n <= 2**i:
+                return 2**i
+
+    def __left(i):
+        return 2*i+1
+
+    def __right(i):
+        return 2*i+2
+
+    def __parent(i):
+        return (i+1)//2-1 if i > 0 else None
+
+
+A = [27, 18, 16, 37, 25, 54, 21, 11]
+st = SegmentTree(Monoid(A, max, 0))
+assert (st.tree == [54, 37, 54, 27, 37, 54,
+        21, 27, 18, 16, 37, 25, 54, 21, 11])
+
+assert (st.answer_range(2, 3) == 16)
+assert (st.answer_range(2, 4) == 37)
+assert (st.answer_range(2, 5) == 37)
+assert (st.answer_range(2, 6) == 54)
+assert (st.answer_range(2, 8) == 54)
+
+st.update(2, 53)
+assert (st.tree == [54, 53, 54, 27, 53, 54,
+        21, 27, 18, 53, 37, 25, 54, 21, 11])
