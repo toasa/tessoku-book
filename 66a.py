57310b77d8adda20c824583a698f2b8b91c54e32
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-03-21 07:51:54 +0900

    WIP: a66, union by size, TLE

--- 66a.py
+++ 66a.py
@@ -1,17 +1,12 @@
-import sys
-sys.setrecursionlimit(10**7)
-
-
 class UnionFinder:
     def __init__(self, n_node):
         self.nodes = [UnionFinder.Node(i) for i in range(n_node+1)]
 
     def __root(self, node):
-        if node.parent == None:
-            return node
-
-        self.parent = self.__root(node.parent)
-        return self.parent
+        cur = node
+        while cur.parent != None:
+            cur = cur.parent
+        return cur
 
     def is_connected(self, id1, id2):
         n1 = self.nodes[id1]
@@ -23,12 +18,17 @@ class UnionFinder:
         r1 = self.__root(self.nodes[n1])
         r2 = self.__root(self.nodes[n2])
 
+        if r1.nchild > r2.nchild:
+            r1, r2 = r2, r1
+
+        r2.nchild += r1.nchild
         r1.parent = r2
 
     class Node:
         def __init__(self, _id):
             self.id = _id
             self.parent = None
+            self.nchild = 1
 
 
 def solve_a66(n_node, queries):
