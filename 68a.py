7ac43916642f67d36d75c0a55bb6046d5d2e4940
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-03-30 18:01:15 +0900

    a68

---
+++ 68a.py
@@ -0,0 +1,85 @@
+class FordFulkerson:
+    def __init__(self, n_node, edge_infos):
+        self.n_node = n_node
+        self.start = 1
+        self.end = n_node
+
+        self.flows = [[0 for _ in range(n_node+10)] for _ in range(n_node+10)]
+        self.adjs = [[] for _ in range(n_node+10)]
+
+        for src, dst, cap in edge_infos:
+            self.flows[src][dst] = cap
+            self.adjs[src].append(dst)
+            self.adjs[dst].append(src)
+
+    def __choose_path(self):
+        stack = [self.start]
+        visited = [False for _ in range(self.n_node+10)]
+        visited[self.start] = True
+
+        chosen_path = []
+
+        while len(stack) > 0:
+            src = stack.pop()
+            chosen_path.append(src)
+
+            if src == self.end:
+                break
+
+            cannot_flow = True
+
+            for dst in self.adjs[src]:
+                if not visited[dst] and self.flows[src][dst] > 0:
+                    stack.append(dst)
+                    visited[dst] = True
+                    cannot_flow = False
+                    break
+
+            if cannot_flow:
+                chosen_path.pop()
+                stack = chosen_path
+
+        path_len = len(chosen_path)
+        if path_len == 0 or chosen_path[path_len-1] != self.end:
+            return 0
+
+        # Calculate minimum amount of flow
+        amount = 2**64
+        for i in range(path_len-1):
+            src, dst = chosen_path[i], chosen_path[i+1]
+            amount = min(amount, self.flows[src][dst])
+
+        # Update residual path
+        for i in range(path_len-1):
+            src, dst = chosen_path[i], chosen_path[i+1]
+            self.flows[src][dst] -= amount
+            self.flows[dst][src] += amount
+
+        return amount
+
+    def solve(self):
+        res = 0
+
+        while True:
+            amount = self.__choose_path()
+            if amount == 0:
+                break
+
+            res += amount
+
+        return res
+
+
+def main():
+    N, M = map(int, input().split())
+
+    edge_infos = []
+    for _ in range(M):
+        src, dst, cap = map(int, input().split())
+        edge_infos.append([src, dst, cap])
+
+    ffa = FordFulkerson(N, edge_infos)
+    print(ffa.solve())
+
+
+main()
