79b6b9a2890f129eaccdf19e89bf7e54450701bf
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2024-04-07 10:27:28 +0900

    Update toybox/sp_delimited_to_com_delimitted.py

--- toybox/sp_delimited_to_com_delimitted.py
+++ toybox/sp_delimited_to_com_delimitted.py
@@ -1,13 +1,8 @@
-import sys
 import re
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Invalid number of parameters")
-        sys.exit(1)
-
-    s = sys.argv[1]
+    s = input()
     print(re.sub("\s+", ",", s.strip()))
 
 
